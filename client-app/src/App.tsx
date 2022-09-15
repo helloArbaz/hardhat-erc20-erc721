1e57d2375fd7c3de87f9046372268d98198c8258
arbaz <arbaz.khan@freecharge.com>
2022-09-15 15:31:17 +0530

    push

---
+++ client-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Box} from "@mui/material"
+
+function App() {
+  return (
+   <Box>
+    Hello From Material UI
+   </Box>
+  );
+}
+
+export default App;
