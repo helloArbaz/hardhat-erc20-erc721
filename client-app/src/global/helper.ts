759567e38cb52ad96fe250debb178715b653655b
arbaz <arbaz.khan@freecharge.com>
2022-09-22 20:27:11 +0530

    working

--- client-app/src/global/helper.ts
+++ client-app/src/global/helper.ts
@@ -1,4 +1,3 @@
-import { ethers } from 'ethers'
 const Lorem = () =>
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloremque aut est animi fuga, quam nam! Voluptatibus, molestiae. Perferendis quo tempora ut maiores enim id minus ullam unde quae aperiam.'
 
